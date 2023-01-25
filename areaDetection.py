1d075494f743af9395cbb3bb70e552057b219657
rockstarcoder333 <successcoder123@gmail.com>
2023-01-25 17:29:09 +0100

    push

--- areaDetection.py
+++ areaDetection.py
@@ -1,6 +1,10 @@
 from paddleocr import PaddleOCR,draw_ocr
 ocr = PaddleOCR(use_angle_cls=True, lang='en') 
 
+## detection
 def detection_text(img):
 	result = ocr.ocr(img, cls=True)
 	return result[0]
+
+def print_data():
+	print("data")
