65be75291a0f4b0eb49af9fb075529bb49f4e58d
rockstarcoder333 <successcoder123@gmail.com>
2023-01-25 14:51:33 +0100

    Update: comment

--- drawPhotoshop_origin.py
+++ drawPhotoshop_origin.py
@@ -13,13 +13,13 @@ def main(input_img, output_psd):
 		new_text_layer = doc.artLayers.add()
 		new_text_layer.kind = ps.LayerKind.TextLayer
 		new_text_layer.textItem.contents = text_item
-		print("content", text_item)
+		print("content is", text_item)
 		new_text_layer.textItem.position = [50, idx * 14 + 20]
 		new_text_layer.textItem.size = 12
 
 	# options = ps.PhotoshopSaveOptions(quality=5)
 	options = ps.PhotoshopSaveOptions()
-	# # save to file
+	# # save to file.
 	doc.saveAs(output_psd, options, asCopy=True)
 
 if __name__ == '__main__':
